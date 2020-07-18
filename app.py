50b5f3d9dada3a0a0b7903bcdaf3e1c3261109ac
onenaghi <onenaghi@yahoo.com>
2020-07-18 18:43:07 -0500

    added python app

---
+++ app.py
@@ -0,0 +1,14 @@
+# Import Dependencies
+import numpy as np
+import pandas as pd
+
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#______________Database Setup______________
